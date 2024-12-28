969fc8239f191f13f5a47b091e38684f27f3cb9d
AnastasiaDeka <99515576+AnastasiaDeka@users.noreply.github.com>
2024-12-28 22:17:26 +0300

    fixed bugs

--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -3,10 +3,10 @@ from django.core.exceptions import ValidationError
 
 from users.constants import DISALLOWED_USERNAMES
 
-def validate_username(value): 
-    """Проверяет, что имя пользователя не входит в список запрещённых.""" 
-    if value.lower() in DISALLOWED_USERNAMES: 
-        raise ValidationError( 
-            f'Имя пользователя "{value}" запрещено для использования.' 
-        ) 
+def validate_username(value):
+    """Проверяет, что имя пользователя не входит в список запрещённых."""
+    if value.lower() in DISALLOWED_USERNAMES:
+        raise ValidationError(
+            f'Имя пользователя "{value}" запрещено для использования.'
+        )
     return value

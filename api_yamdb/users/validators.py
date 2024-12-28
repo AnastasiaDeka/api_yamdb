cabc12174923a50a5ffa969e6a46caba14137f3f
AnastasiaDeka <99515576+AnastasiaDeka@users.noreply.github.com>
2024-12-28 22:18:23 +0300

    fixed bugs

--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -3,6 +3,7 @@ from django.core.exceptions import ValidationError
 
 from users.constants import DISALLOWED_USERNAMES
 
+
 def validate_username(value):
     """Проверяет, что имя пользователя не входит в список запрещённых."""
     if value.lower() in DISALLOWED_USERNAMES:
